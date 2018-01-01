20a7a8309e140789073a84c557dddd700fd0df01
sebastian Higuita castañeda <sebastianhiguitaca@gmail.com>
2018-01-01 14:48:59 -0500

    vue employee manager

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDiGhwLzI-YpDqAHFB_DB3x111jK2N-LjM",
+    authDomain: "fir-global-a3068.firebaseapp.com",
+    databaseURL: "https://fir-global-a3068.firebaseio.com",
+    projectId: "fir-global-a3068",
+    storageBucket: "fir-global-a3068.appspot.com",
+    messagingSenderId: "273497438908"
+}
